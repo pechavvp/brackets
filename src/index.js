46ebcb135c32e9f9060af7af7236fae1fb93e97e
VVP <pechathebest@yandex.ru>
2022-02-24 17:21:34 +0300

    fix bag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-let count = 0;
-
 module.exports = function check(str, bracketsConfig) {
+  let count = 0;
   for (let i=0; i<bracketsConfig.length; i++) {
     let oneItem = bracketsConfig[i][0] + bracketsConfig[i][1];
     if (str.includes(oneItem)) {
