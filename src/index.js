48065cc40cd96d502bdcc0e6956a084d7a182a40
VVP <pechathebest@yandex.ru>
2022-02-24 16:17:07 +0300

    feat: done not all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+let count = 0;
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i=0; i<bracketsConfig.length; i++) {
+    let oneItem = bracketsConfig[i][0] + bracketsConfig[i][1];
+    if (str.includes(oneItem)) {
+      count +=1;
+      let index = str.indexOf(oneItem);
+      console.log(index);
+      let str1 = str.slice(0, index);
+      let str2 = str.slice(index+2);
+      str = str1+str2
+      console.log(str);
+    }
+  }
+  if (count == 0) {
+    return false;
+  } else if (str.length == 1) {
+    return false;
+  } else if (str.length == 0) {
+    return true;
+  } else {
+    check(str, bracketsConfig);
+  }
 }
